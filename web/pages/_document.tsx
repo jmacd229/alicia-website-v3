666c47fa7d59a0519378b19a7f5052e481331ebc
jmacd229 <jmacd229@uwo.ca>
2023-06-04 13:50:00 -0400

    Fixes GTM integration

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -5,9 +5,13 @@ import Document, {
   NextScript,
   DocumentContext,
 } from "next/document";
-import Script from "next/script";
-import { ServerStyleSheet } from "styled-components";
+import styled, { ServerStyleSheet } from "styled-components";
 import colors from "styles/palette";
+
+const GoogleTagManager = styled.iframe`
+  display: none;
+  visibility: hidden;
+`;
 
 // Taken from https://reacthustle.com/blog/nextjs-styled-components-ssr-typescript
 export default class MyDocument extends Document {
@@ -39,11 +43,6 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <Script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-N84THGP');`}</Script>
           <meta name="theme-color" content={colors.grey.a} />
           <link
             rel="preload"
@@ -71,12 +70,13 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           />
         </Head>
         <body>
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N84THGP"
-      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>`,
-            }}
-          />
+          <noscript>
+            <GoogleTagManager
+              src="https://www.googletagmanager.com/ns.html?id=GTM-N84THGP"
+              height="0"
+              width="0"
+            />
+          </noscript>
           <Main />
           <NextScript />
         </body>
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -10,7 +10,7 @@ import GlobalStyle from "styles/GlobalStyle";
 import mailgo from "mailgo";
 import Footer from "sections/Footer";
 import Head from "next/head";
-import { PrivacyPolicy } from "sections/PrivacyPolicy";
+import Script from "next/script";
 
 const App = ({
   sections,
@@ -25,6 +25,15 @@ const App = ({
   }
   return (
     <>
+      <Script
+        id="google-tag-manager"
+        strategy="afterInteractive"
+        onLoad={() => console.log("hello")}
+      >{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N84THGP');`}</Script>
       <Head>
         <title>Dr. Alicia MacDougall</title>
         <meta
