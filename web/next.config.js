0a628ff217bee4c854b3d9b01e132c4409463788
jmacd229 <jessemacdgl@gmail.com>
2025-02-12 22:10:18 -0500

    Updates Node and adds ESLint and prettier config

--- web/next.config.js
+++ web/next.config.js
@@ -1,19 +1,22 @@
 module.exports = {
-	compiler: {
-		styledComponents: true
-	},
-	webpack(config) {
-		config.module.rules.push({
-			test: /\.svg$/i,
-			issuer: /\.[jt]sx?$/,
-			use: [{
-				loader: '@svgr/webpack', options: {
-					typescript: true,
-					ext: "tsx",
-				}
-			}],
-		})
+  compiler: {
+    styledComponents: true,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            typescript: true,
+            ext: "tsx",
+          },
+        },
+      ],
+    });
 
-		return config
-	},
-}
+    return config;
+  },
+};
